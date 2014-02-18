4192d2a25cf8f4797d1b14e7200236d76f524327
Daniel Aranda <javierdaniel@gmail.com>
2014-02-17 19:29:42 -0500

    removing generator files in order to use grunt

--- app/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('APP_PATH', __DIR__ . '/');
-
-require APP_PATH . 'core/FileUnifier.php';
-require APP_PATH . 'core/JavaScript.php';
-require APP_PATH . 'core/HTML.php';
-require APP_PATH . 'core/CSS.php';
-require APP_PATH . 'core/Templates.php';
