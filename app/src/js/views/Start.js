5c33701a3ed080574a5aad86149acc2a189ce2a9
Daniel Aranda <javierdaniel@gmail.com>
2014-03-15 20:42:38 -0400

    progress with the layout

--- app/src/js/views/Start.js
+++ app/src/js/views/Start.js
@@ -11,23 +11,12 @@ $w.views.Start = $w.views.Abstract.extend({
 
     addNewChart : function(){
         var c = new $w.collections.charts.UserChart();
-        var u = $w.Application.user().get('id');
-        var newChart = c.add({user_id :u, bonita:'Lily' })[0];
+        var newChart = c.add({})[0];
+
     },
 
     afterRender : function(){
         this._super();
-        //$w.Application.fireBase().child('charts').child('chart').set({user_id : uid, name : 'daniel'});
-
-//        var b = $w.Application.fireBase();
-//        b.child('charts').push({user_id : uid, name : 'daniel'});
-//
-//        b.child('charts').endAt().limit(10).on('child_added', function(snapshot) {
-//            console.log(snapshot.val().name)
-//        })
-
-//        Firebase.goOnline()
-
     }
  
 });
