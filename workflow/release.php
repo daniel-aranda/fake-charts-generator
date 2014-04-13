b4946daacb6b50de656bc43ccc0e5a3b7fb1a6f1
Daniel Aranda <javierdaniel@gmail.com>
2014-04-13 15:08:33 -0400

    release command ready

--- workflow/release.php
+++ workflow/release.php
@@ -2,8 +2,8 @@
 require __DIR__ . '/workflow.php';
 
 $public_root = dirname(__DIR__) . '\\public';
-
 $deploy_dir = dirname(dirname(dirname(__DIR__))) . '/deploys/fake-charts-generator';
+$version = version();
 
 if( !is_dir($deploy_dir) ){
     abort('deploy dir is not yet created: ' . PHP_EOL . $deploy_dir);
@@ -14,9 +14,16 @@ write_line('initializing');
 chdir($deploy_dir);
 
 exec("git checkout -f gh-pages");
-write_line(exec("git status"));
+exec("git clean -df");
+exec("git status",$output);
 
 write_line(exec("xcopy \"$public_root\" \"$deploy_dir\" /c /q /e /y"));
 
+exec("git add . && git add -u");
 exec("git status",$output);
 echo implode(PHP_EOL, $output);
+
+exec('git commit -m "deploying version '.$version.'"');
+exec('git push');
+
+write_line('deploy finished');
