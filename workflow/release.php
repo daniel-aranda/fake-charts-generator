89499ddb759fd1cd8cd962f5e0438067ad478948
Daniel Aranda <javierdaniel@gmail.com>
2014-04-13 14:24:45 -0400

    progress with release command. Files copied from public folder. Removing .htaccess

--- workflow/release.php
+++ workflow/release.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/workflow.php';
 
-$public_root = dirname(__DIR__) . '/public/';
+$public_root = dirname(__DIR__) . '\\public';
 
 $deploy_dir = dirname(dirname(dirname(__DIR__))) . '/deploys/fake-charts-generator';
 
@@ -10,5 +10,13 @@ if( !is_dir($deploy_dir) ){
 }
 
 write_line('initializing');
-write_line(exec("git checkout -f gh-pages", $output));
+
+chdir($deploy_dir);
+
+exec("git checkout -f gh-pages");
 write_line(exec("git status"));
+
+write_line(exec("xcopy \"$public_root\" \"$deploy_dir\" /c /q /e /y"));
+
+exec("git status",$output);
+echo implode(PHP_EOL, $output);
