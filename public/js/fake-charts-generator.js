729b5cb6fcc6a2ccfedf09e705b7a037a9eb9a0d
Daniel Aranda <javierdaniel@gmail.com>
2014-10-12 18:00:26 -0400

    area chart done

--- public/js/fake-charts-generator.js
+++ public/js/fake-charts-generator.js
@@ -2010,21 +2010,26 @@ $w.views.charts.Area = $w.views.charts.Abstract.extend({
 
     active: true,
     chartNode: null,
+    template: 'charts_area',
+    className: 'chart-component area',
 
     afterRender : function(){
         this._super();
 
         this.chart = new Rickshaw.Graph( {
-            element: this.$el[0],
-            width: 700,
+            element: this.$('.chart')[0],
+            width: 600,
             height: 400,
             renderer: Rickshaw.Graph.Renderer.AnimatedArea,
             stroke: true,
-            series: [ {
+            series: [{
                 data: this.randomData(),
                 color: '#cae2f7'
-            } ]
+            }],
+            padding: {top: 0.2}
         } );
+        this.addXAxis();
+        this.addYAxis();
         this.chart.render();
         this.looper();
     },
@@ -2037,10 +2042,38 @@ $w.views.charts.Area = $w.views.charts.Abstract.extend({
     randomData: function(){
         return [
             { x: 0, y: Math.round(Math.random() * 50)},
-            { x: 0, y: Math.round(Math.random() * 50)},
-            { x: 0, y: Math.round(Math.random() * 50)},
-            { x: 1, y: Math.round(Math.random() * 50) }
+            { x: 1, y: Math.round(Math.random() * 50)},
+            { x: 2, y: Math.round(Math.random() * 50)},
+            { x: 3, y: Math.round(Math.random() * 50) },
+            { x: 4, y: Math.round(Math.random() * 50) },
+            { x: 5, y: Math.round(Math.random() * 50) },
+            { x: 6, y: Math.round(Math.random() * 50) },
+            { x: 7, y: Math.round(Math.random() * 50) },
+            { x: 8, y: Math.round(Math.random() * 50) }
         ];
+    },
+
+    addXAxis : function(){
+        this.xAxis = new Rickshaw.Graph.Axis.X({
+            graph : this.chart,
+            orientation: 'bottom',
+            element: this.$('.x-axis')[0],
+            tickValues : [0, 2, 4, 6, 8]
+        });
+
+        this.xAxis.render();
+    },
+
+    addYAxis : function(){
+        this.yAxis = new Rickshaw.Graph.Axis.Y({
+            graph : this.chart,
+            tickFormat : d3.format("$"),
+            orientation: 'left',
+            element: this.$('.y-axis')[0]
+        });
+
+        this.yAxis.render();
+
     }
 
 });
